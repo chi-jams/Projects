78b4af32e3c0eae9753a518fc3cc6e3e1cf1cfb5
daichij <djameson@mines.edu>
2017-10-10 15:34:04 -0600

    Got Decoding/Encoding working in go

--- img_scale/img_scale.py
+++ img_scale/img_scale.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
-from PIL import Image
+import pyscreenshot as ImageGrab
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./img_scale.py <input_file> <output_file>")
+    if len(sys.argv) != 2:
+        print("Usage: ./img_scale.py <output_file>")
         exit(-1)
-    Image.open(sys.argv[1]).save(sys.argv[2])
+    ImageGrab.grab().save(sys.argv[1])
