fd87e9b8cb94a9dbc67e4538b971bb99071e3cbb
daichij <djameson@mines.edu>
2017-10-31 10:23:43 -0600

    Got rid of img_scale from this repo since it has its own, and started a rust sandbox

--- img_scale/img_scale.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import sys
-import pyscreenshot as ImageGrab
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./img_scale.py <output_file>")
-        exit(-1)
-    ImageGrab.grab().save(sys.argv[1])
---
+++ rust/cake/target/debug/cake.d
@@ -0,0 +1,1 @@
+/home/daichij/Projects/rust_puppy/cake/target/debug/cake: /home/daichij/Projects/rust_puppy/cake/src/main.rs
