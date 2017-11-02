a48ad87ba608f4d48e986b1683d48527f057f325
daichij <djameson@mines.edu>
2017-11-02 11:42:30 -0600

    Removed target directory from repo, started messing around with Rust

--- rust/cake/src/main.rs
+++ rust/cake/src/main.rs
@@ -1,3 +1,28 @@
+
+use std::{env, process};
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage {} <in_file> <out_file>", args[0]);
+        process::exit(-1);
+    }
+
+    println!("Opening {}...", args[1]);
+    let contents = get_file_contents(&args[1]);
+
+    let mut out_file = File::create(&args[2]).unwrap();
+    out_file.write_fmt(format_args!("{}", contents)).unwrap();
 }
+
+fn get_file_contents(file_name: &String) -> String{
+    let mut file = File::open(&file_name).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    contents
+}
+
--- rust/cake/target/debug/cake.d
+++
@@ -1,1 +0,0 @@
-/home/daichij/Projects/rust_puppy/cake/target/debug/cake: /home/daichij/Projects/rust_puppy/cake/src/main.rs
