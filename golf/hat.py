44162c25330843da8505b4378bcd7dc713ac80f2
daichij <djameson@mines.edu>
2017-11-16 12:16:10 -0700

    Did a couple of code-golf problems

---
+++ golf/hat.py
@@ -0,0 +1,4 @@
+#68c,
+#https://codegolf.stackexchange.com/questions/97456/draw-an-alphabet-party-hat
+i=25
+exec'print" "*(i/2)+"abcdefghijklmnopqrstuvwxyz"[i:];i-=1;'*26
---
+++ golf/staircase.py
@@ -0,0 +1,3 @@
+#41c,
+#https://codegolf.stackexchange.com/questions/147469/alphabet-staircase
+for i in range(26):print chr(97+i)*(i+1)
