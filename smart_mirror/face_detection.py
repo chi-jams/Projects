24ef67bf1bbb003bbe99cf838e547eea8b69ced0
daichij <djameson@mines.edu>
2017-12-27 23:48:45 -0700

    Started working with camshift

--- smart_mirror/face_detection.py
+++ smart_mirror/face_detection.py
@@ -2,25 +2,57 @@
 import numpy as np
 import cv2
 
+usingHaar = False 
 cap = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+# Initial window location
+r,h,c,w = 250,90,400,125
+track_window = (c,r,w,h)
+
+# Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+
 while True:
     ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if usingHaar:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+    else:
+        # set up the ROI for tracking
+        roi = frame[r:r+h, c:c+w]
+        hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+        roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+        cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+
+        # apply meanshift to get the new location
+        ret, track_window = cv2.CamShift(dst, track_window, term_crit)
+
+        # Draw it on image
+        pts = cv2.boxPoints(ret)
+        pts = np.int0(pts)
+        #frame = cv2.polylines(frame,[pts],True, 255,2) 
+        frame = cv2.polylines(hsv,[pts],True, 255,2) 
 
     cv2.imshow('frame', frame) 
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    keyPress = cv2.waitKey(1)
+    if keyPress == ord('q'):
         break
+    elif keyPress == ord('s'):
+        usingHaar = not usingHaar
+        print("Using Haar: {}".format(usingHaar))
 
 cap.release()
 cv2.destroyAllWindows()
