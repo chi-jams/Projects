daa5cc2cc0ed3cfb854e97d46305612395a7cff8
daichij <djameson@mines.edu>
2018-05-19 23:39:05 -0600

    Got a serial version of TSP working

--- TSP/TSP.cu
+++ TSP/TSP.cu
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <limits.h>
+#include <math.h>
+
+#include <algorithm>
 
 int rand_range(int min, int max);
 int* make_pts(int N);
+int* gen_perm(int n, int k);
+
+double get_dist(const int* pts, int i, int j);
+
+void serial_tsp(int* pts, double& min_dist, int& min_perm, int N, int Nf);
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -23,17 +31,26 @@ int main(int argc, char* argv[]) {
     }
 
     printf("N: %d\n", N);
+    unsigned long long int Nf = 1;
+    for (int i=N;i>0;i--) Nf*=i;
 
     int* pts = make_pts(N);
 
     for (int i = 0; i < 2 * N; i+=2)
         printf("%d: (%d, %d)\n", i / 2, pts[i], pts[i + 1]);
 
-    int* perms = (int*) malloc(sizeof(int) * N);
-    for (int i = 0; i < N; i++) perms[i] = i;
+    double min_dist;
+    int min_perm;
+    serial_tsp(pts, min_dist, min_perm, N, Nf);
+    printf("Min distance: %f\n", min_dist);
+    printf("Path:\n");
+    int* perm = gen_perm(N, min_perm);
+    for (int i = 0; i < N; i++) {
+        int p_i = perm[i];
+        printf("%d: (%d, %d)\n", p_i, pts[2*p_i], pts[2*p_i + 1]);
+    }
 
     free(pts);
-    free(perms);
     return 0;
 }
 
@@ -51,4 +68,57 @@ int* make_pts(int N) {
     }
 
     return pts;
+}
+
+int* gen_perm(int n, int perm) {
+    int i, ind, m=perm;
+    int* p = (int*) malloc(sizeof(int) * n);
+    int* e = (int*) malloc(sizeof(int) * n);
+
+    for (i=0;i<n;i++)e[i]=i;
+    for (i=0;i<n;i++) {
+        ind = m % (n - i);
+        m = m / (n - i);
+        p[i] = e[ind];
+        e[ind] = e[n - i - 1];
+    }
+
+    free(e);
+
+    return p;
+}
+
+double get_dist(const int* pts, int i, int j) {
+    int x1 = pts[2*i],
+        y1 = pts[2*i + 1],
+        x2 = pts[2*j],
+        y2 = pts[2*j + 1];
+
+    int dx = x1 - x2,
+        dy = y1 - y2;
+    dx *= dx;
+    dy *= dy;
+    
+    return sqrt(dx + dy);
+}
+
+void serial_tsp(int* pts, double& min_dist, int& min_perm, int N, int Nf) {
+    min_dist = -1;
+    int* perm;
+
+    for (int i = 0; i < Nf; i++) {
+        perm = gen_perm(N, i); 
+
+        double dist = 0;
+        for (int j = 0; j < N; j++) {
+            dist += get_dist(pts, perm[j], perm[(j+1) % N]);
+        }
+
+        if (dist < min_dist || min_dist < 0) {
+            min_dist = dist;
+            min_perm = i;
+        }
+        
+        free(perm);
+    }
 }
