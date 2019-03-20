70977c2767d2f02abdfcfcb1fbc213ded13b70b5
daichij <djameson@mines.edu>
2019-03-19 19:10:07 -0600

    Worked on webgl stuff some more

--- WebGL/hurr.js
+++ WebGL/hurr.js
@@ -1,8 +1,33 @@
 
 main();
 
+// Compile and link a full shader program and return it
+function initShaderProgram(gl, shaders) {
+    shaders.forEach(function(el, wha) {
+        console.log(el, wha);
+    });
+}
+
+// Compile a single shader in a pipeline 
+function loadShader(gl, type, src) {
+    
+    const shader = gl.createShader(type);
+
+    gl.shaderSource(shader, src);
+
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('Compilation error on shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+
+        return null;
+    }
+
+    return shader;
+}
+
 function main() {
-    console.log("what");
     const canvas = document.querySelector("#glCanvas");
 
     const gl = canvas.getContext("webgl");
@@ -15,4 +40,12 @@ function main() {
     gl.clearColor(0,0,0,1);
     
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    shaders = [
+        {type: gl.VERTEX_SHADER, src: "uhh"},
+        {type: gl.FRAGMENT_SHADER, src: "wut"}
+    ];
+
+    initShaderProgram(gl, shaders);
+
 }
